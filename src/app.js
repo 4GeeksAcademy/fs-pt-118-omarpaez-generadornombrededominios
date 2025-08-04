5abe00d511f63e83e4e6661e3e41d8d0ee5f2f1a
Omar Páez <135053433+omarpaezdev@users.noreply.github.com>
2025-08-04 19:46:56 +0000

    feat: Proyecto final Generador de Dominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let ext = ['.com','.net', '.us', '.io']  // se crea un nuevo array con las posibles extensiones
+
+let domainTable = [] // array que almacenara los nomres de dominios resultantes
+let tableHtml=""
+
+// se implementan 4 for of para cada uno de los arreglos que contiene las diferentes partes del nombre 
+for (let valPronoun of pronoun) {
+  for (let valAdj of adj) {
+    for (let valNoun of noun) {
+      for (let valExt of ext) {
+       // console.log(valPronoun + valAdj + valNoun + valExt)
+        domainTable.push(`${valPronoun}${valAdj}${valNoun}${valExt}`) //hacemo push por cada iteracion concatenando las diferentes combinaciones
+       }
+    }
+  }
+}
+
+// con este for recorremos el array que contiene los nombres resultantes, para ir acumulando en una variable string, para pasarla a una tabla y poder verla en html
+for(let i=0; i<domainTable.length; i++){
+  tableHtml += `<tr><th>${i+1}</th><td>${domainTable[i]}</td></tr>`
+}
+
+//console.table(domainTable)
+//console.log(tableHtml)
+
+
+ let d = document
+ d.getElementById("nameDomains").innerHTML = tableHtml
